3d6b8cd11cd2a46c7c7b53c60e3819489f6052d9
Beatriz Miranda <163881019+Biamiranda27@users.noreply.github.com>
2025-03-22 21:06:04 -0300

    Add files via upload

---
+++ PalindromoR.java
@@ -0,0 +1,38 @@
+/*Crie um método recursivo que recebe uma string como parâmetro e retorna true se 
+essa é um ``Palíndromo''. Na saída padrão, para cada linha de entrada, escreva uma 
+linha de saída com SIM/NÃO indicando se a linha é um palíndromo. Destaca-se que uma 
+linha de entrada pode ter caracteres não letras. */
+
+import java.util.Scanner;
+
+class Testar{
+    public boolean p(String pal){
+      return p(pal, 0, pal.length() -1);
+}
+public boolean p(String pal, int esquerda, int direita){
+    if(esquerda >= direita){
+        return true;
+    } if(pal.charAt(esquerda) != pal.charAt(direita)){
+        return false;
+    }
+    return p(pal, esquerda +1, direita-1);
+ }
+}
+public class PalindromoR {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Testar a = new Testar();
+        String palavra;
+        while(true){
+            // System.out.println("digite uma palavra");
+             palavra = scanner.nextLine(); //metodo para ler string
+             if(palavra.equals("FIM")){ 
+                 break;
+             } else if(a.p(palavra)){
+                 System.out.println("SIM");
+             } else {
+               System.out.println("NAO");;
+             }
+         }  scanner.close();
+    }
+}
